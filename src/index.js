61a2c4699c19da286378af9955e38dc8d9573747
Ricardo Manso <ricardoltm@gmail.com>
2021-12-05 18:32:09 -0300

    Solution implemented

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function checksExistsUserAccount(request, response, next) {
 
   const user = users.find((user) => user.username === username);
   if (!user) {
-    return response.status(400).json({
+    return response.status(404).json({
       error: "User not found.",
     });
   }
@@ -25,15 +25,52 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if (user.pro) return next();
+
+  if (user.todos.length === 10) {
+    return response.status(403).json({ erro: "Todo limit reached." });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Todo ID is not uuid." });
+  }
+
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  const todo = user.todos.find((todo) => (todo.id = id));
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found." });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+  if (!user) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post("/users", (request, response) => {
